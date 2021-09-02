2a48b48549abb2dfd548c4a09c02f19c57985e7a
RCass172 <ruthcassidy1221@gmail.com>
2021-09-02 18:23:07 +0000

    Add login functionality

--- app.py
+++ app.py
@@ -25,6 +25,33 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if the username entered
+        # already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Ensure the password matches
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
+            else:
+                # Invalid password
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # Username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
+
+
 @app.route("/recipe_categories")
 def recipe_categories():
     categories = list(mongo.db.categories.find().sort("category_name", 1))
