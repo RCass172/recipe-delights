01ba8307e876ec9909bd14b2613b78c24211ec40
RCass172 <ruthcassidy1221@gmail.com>
2021-09-10 13:30:33 +0000

    Add user recipes to profile

--- app.py
+++ app.py
@@ -88,8 +88,10 @@ def profile(username):
         {"username": session["user"]})["username"]
 
     if session["user"]:
-        return render_template("profile.html", username=username)
-
+        user_recipes = list(
+            mongo.db.recipes.find({"created_by": session["user"]}))
+        return render_template(
+            "profile.html", username=username, user_recipes=user_recipes)
     return redirect(url_for("login"))
 
 
