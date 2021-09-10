f4542010c96ea859ab631cbd9ffbea14b86e4417
RCass172 <ruthcassidy1221@gmail.com>
2021-09-10 16:05:05 +0000

    Update add ingredient button to dynamically add new input field

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('.collapsible').collapsible();
     $('select').formSelect();
-  });
+});
+
+// Used following website to help create dynamic add button 
+// https://jquerylove.com/content/how-to-use-jquery-to-dynamically-add-text-field-in-form-2004/
+let ingredient = 1;
+
+    $(".add-ingredient").click(function (e) {
+        e.preventDefault();
+            ingredient++;
+            $(".new-ingredient").append(`
+            <div class="input-field">
+            <input id="ingredients${ingredient}" name="ingredients" type="text" class="validate" minlength="3" required>
+            <label for="ingredients${ingredient}">Add Another Ingredient</label></div>`);
+    });
